7f1799b4e7669a35baaeb98fcfa95bced88f201d
Prathamesh0506-spec <prathameshohol12@gmail.com>
2025-04-22 23:39:51 +0530

    lecture code

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ newHeading.innerHTML = "<i>Hi i am new</i>";
 document.querySelector("body").prepend(newHeading);
 
 
+
+
+/* LECTURE CODE*/
+let div = document.querySelector("#box");
+div.append(newBtn);
+
+
+
 /*DOM PART 2 PRCTISE QUESTIONS*/
 /*Q.1 
 MAKE A BUTTON
 CLICK ME !!
 */
 
-let newBtn = document.createElement("button");
+/*let newBtn = document.createElement("button");
 newBtn.innerText = "click me!!";
 
 newBtn.style.color = "white";
@@ -20,6 +28,6 @@ newBtn.style.backgroundColor = "red";
 
 document.querySelector("body").prepend(newBtn);
 
-
+*/
 
 /*Q.2*/
