3c0475611bc2391ded08b442439c1205d8bbe839
Prathamesh0506-spec <prathameshohol12@gmail.com>
2025-04-25 23:15:47 +0530

    added pracstise Q

--- script.js
+++ script.js
@@ -125,3 +125,23 @@ let = button.addeventListener("click", ()=>{
 });
 */
 
+
+
+//PRACTISE QUESTION Q.1
+//CREATE A BUTTON WHO WILL CHANGE BACKGROUND COLOUR
+
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";
+
+modeBtn.addEventListener("click", ()=>{
+    if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+    }
+    else {
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+})
+
+console.log("currMode");
