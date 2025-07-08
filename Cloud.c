0c2ef54d11d30ace0c7b6ec43812247299cc1abe
Prathamesh0506-spec <prathameshohol12@gmail.com>
2025-07-08 19:43:37 +0530

    reverseStringByStk

--- Cloud.c
+++ Cloud.c
@@ -117,3 +117,57 @@ void pop (){
 
     }
 }
+
+
+
+
+/**********REVERRSE STRING BY STACK PROGRAMM***********/
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+char stk[11],rev[11];
+char n;
+int top=-1,k=0;
+
+
+int main()
+{
+    char name[11];
+    //TAKING INPUT OF NAME
+    printf("Enter A Name\n");
+    scanf("%s", &name);
+
+    //LOGIC FOR PUSHING IN METHOD
+    for(int i=0; name[i]!='\0'; i++){
+        push(name[i]);
+    }
+
+    //LOGIC FOR PUSHING IN POP
+    for(int i = 0; i<strlen(name); i++){
+        pop();
+
+    }
+    //PRINTING
+    printf("The original Name is %s\n", stk);
+    printf("The Reversed Name is %s\n", rev);
+
+    return 0;
+}
+//LOGIC FOR PUSH function
+void push(char n){
+    if(top == 10){
+        printf("STACK-OVERFLOW");
+    }
+    else{
+        top++;
+        stk[top] = n;
+    }
+}
+
+//LOGIC FOR pop
+void pop (){
+    rev[k]=stk[top--];
+    k++;
+}
+
