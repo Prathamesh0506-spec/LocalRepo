96895fc8e6e85ce34a64f8db1dd386c26ed326c3
Prathamesh0506-spec <prathameshohol12@gmail.com>
2025-07-18 22:50:45 +0530

    Imp_Question

--- Cloud.c
+++ Cloud.c
@@ -259,6 +259,130 @@ struct Node *temp;
 
 
 
+/*****************************->->->->->-> ADDITION POLYNOMIALS BY USING <-<-<-<-<-<-<-******************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    float coeff;
+    int expo;
+    struct node *next;
+};
+
+// INSERT FUNCTION
+struct node* insert(struct node* head, float coef, int exp) {
+    struct node* temp;
+    struct node* newp = malloc(sizeof(struct node));  // Fixed malloc type
+
+    newp->coeff = coef;
+    newp->expo = exp;
+    newp->next = NULL;
+
+    if (head == NULL || exp > head->expo) {
+        newp->next = head;
+        head = newp;
+    } else {
+        temp = head;
+        while (temp->next != NULL && temp->next->expo >= exp) {
+            temp = temp->next;
+        }
+        newp->next = temp->next;
+        temp->next = newp;
+    }
+    return head; // moved this outside the else to ensure always returned
+}
+
+// CREATE FUNCTION
+struct node* create(struct node* head) {
+    int n, i;
+    float coeff;
+    int expo;
+
+    printf("Enter Number Of terms\t");
+    scanf("%d", &n);  // Fixed: &n instead of n
+
+    for (i = 0; i < n; i++) {
+        printf("Enter coefficient for term %d\t", i + 1);
+        scanf("%f", &coeff); // Fixed: %f for float
+
+        printf("Enter Exponent for term %d\t", i + 1);
+        scanf("%d", &expo);
+
+        head = insert(head, coeff, expo);
+    }
+    return head;
+}
+
+// DISPLAY FUNCTION
+void print(struct node* head) {
+    if (head == NULL) {
+        printf("EMPTY POLYNOMIAL\n");  // Fixed: changed print to printf
+    } else {
+        struct node* temp = head;
+        while (temp != NULL) {
+            printf("%.1fx^%d", temp->coeff, temp->expo);  // Fixed syntax
+            temp = temp->next;
+            if (temp != NULL) {
+                printf(" + ");
+            } else {
+                printf("\n");
+            }
+        }
+    }
+}
+
+// ADDITION FUNCTION
+void polyAdd(struct node* head1, struct node* head2) {
+    struct node* ptr1 = head1;
+    struct node* ptr2 = head2;
+    struct node* head3 = NULL;
+
+    while (ptr1 != NULL && ptr2 != NULL) {
+        if (ptr1->expo == ptr2->expo) {
+            head3 = insert(head3, ptr1->coeff + ptr2->coeff, ptr1->expo);  // Fixed line
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        } else if (ptr1->expo > ptr2->expo) {
+            head3 = insert(head3, ptr1->coeff, ptr1->expo);
+            ptr1 = ptr1->next;
+        } else {
+            head3 = insert(head3, ptr2->coeff, ptr2->expo);
+            ptr2 = ptr2->next;
+        }
+    }
+
+    while (ptr1 != NULL) {
+        head3 = insert(head3, ptr1->coeff, ptr1->expo);
+        ptr1 = ptr1->next;
+    }
+
+    while (ptr2 != NULL) {
+        head3 = insert(head3, ptr2->coeff, ptr2->expo);
+        ptr2 = ptr2->next;
+    }
+
+    printf("Added polynomial is:\n");
+    print(head3); // Fixed: replaced printf(head3) with print()
+}
+
+// MAIN FUNCTION
+int main() {
+    struct node* head1 = NULL;
+    struct node* head2 = NULL;
+
+    printf("Enter 1st polynomial\n");
+    head1 = create(head1);
+
+    printf("Enter 2nd polynomial\n");
+    head2 = create(head2);
+
+    polyAdd(head1, head2);
+
+    return 0;
+}
+
+
 
 
 
