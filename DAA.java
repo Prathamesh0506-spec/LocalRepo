91e2848c45b854782a2044f9a441cdb92d4cefff
Prathamesh0506-spec <prathameshohol12@gmail.com>
2026-07-12 12:44:12 +0530

    DAA_3

--- DAA.java
+++ DAA.java
@@ -84,3 +84,85 @@ public class allPaths {
     }
 }
 */
+
+
+
+//**********CYCLE IN DIRECTED GRAPH ***********/
+
+/* import java.util.ArrayList;
+
+public class cycleInDirected {
+    static class edge{
+        int src;
+        int dest;
+
+        public edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<edge> graph[] ){
+        ///INTIALIZING LIST OF LIST'S 
+        for(int i = 0; i<graph.length; i++){
+            graph[i] = new ArrayList<edge>();
+        }
+        graph[0].add(new edge(0, 2));
+        graph[1].add(new edge(1, 0));
+        graph[2].add(new edge(2, 3));
+        graph[3].add(new edge(3, 0));
+    }
+
+    //LOGIC FOR CHKING IS CYCLE IS PRESENT
+    public static boolean isCycleDirected(ArrayList<edge>graph[], boolean vis[], int curr, boolean rec[]){
+        vis[curr] = true;
+        rec[curr] = true;
+
+        for(int i = 0; i<graph[curr].size(); i++){      //TYA NODE CHE SAGLE NEIGHB VISIT HOTAT
+            edge e = graph[curr].get(i);        //NODE KADHUN GHENE & EDGE STORE KARNE
+
+            //CORNER CASE 1----> TYA NODE CHA NEIGH CYCYLE MADHE ASLYAS
+            if(rec[e.dest]){
+                return true;
+            }
+
+            //CORNER CASE 2----> CODE KARAYCHI GARAJ NAHI
+
+
+            //CORNER CASE 3----> KONI TARI AADHICH VISIT KELELA NEIGH ASEL TAR
+            else if(!vis[e.dest]){
+                if(isCycleDirected(graph, vis, e.dest, rec)){
+                    return true;
+                }
+            }
+        }
+            rec[curr]  = false;
+            return false;
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        boolean vis [] = new boolean[V];
+        boolean rec [] = new boolean[V];
+
+         boolean isCycle = false;
+
+        //
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+                if(isCycleDirected(graph, vis, i, rec)){
+                    isCycle = true;
+                    break;
+                }
+            }
+        }
+                System.out.println("Cycle Present: " + isCycle);
+
+    }
+}
+
+
+*/
